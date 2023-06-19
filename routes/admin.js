df9e954ae034bb4d2ae7069fbc1e44996809ab0d
nguyen <nguyentk1272@gmail.com>
2023-06-19 21:39:31 +0700

    first commit

---
+++ routes/admin.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var name;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('admin', { title: 'Admin Page', name:"trong" });
+});
+
+module.exports = router;
