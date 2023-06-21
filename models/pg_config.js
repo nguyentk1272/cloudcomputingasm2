74fb574a7e9fec4567fd6bd09b1f388dc1863aca
nguyen <nguyentk1272@gmail.com>
2023-06-21 15:21:00 +0700

    update 2.0

--- models/pg_config.js
+++ models/pg_config.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pg_conn = new Pool({
-  user: "fgtpuqlsbtvkkf",
-  host: "ec2-3-216-167-65.compute-1.amazonaws.com",
-  database: "dprdjhoipgf3p",
-  password: "1f49fc30349740099fe9eec83368e7d5b772f514caf26c50eae6c919bd79b943",
-  port: 5432,
+  user: "postgres",
+  host: "containers-us-west-89.railway.app",
+  database: "postgres",
+  password: "adc72Fb7NE5f9jJ2LhKI",
+  port: 7770,
   ssl: {
     rejectUnauthorized: false,
   },
